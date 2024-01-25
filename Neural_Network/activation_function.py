ebc348dda6e3e58726313358d1801ee78093aee9
tdp1996 <phuongtd1107@gmail.com>
2024-01-25 22:25:40 +0700

    refactor linear class

--- Neural_Network/activation_function.py
+++ Neural_Network/activation_function.py
@@ -2,7 +2,8 @@ class ReLU():
     def __init__(self):
         pass
     def relu(self,input):
-        if isinstance(self.input,list):
+        self.input = input
+        if isinstance(input,list):  
             return [pre_single_act_node if pre_single_act_node > 0  else 0 for pre_single_act_node in input]
         else:
             return self.input
@@ -14,4 +15,8 @@ class ReLU():
             return self.relu(self.input)
         
         
-    
+if __name__ == "__main__":
+    relu = ReLU()
+    output = relu([2,2,2])
+    print(output)
+
